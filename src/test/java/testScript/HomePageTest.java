b51ce211523c7c760ef45fe6d3a735616eeb52de
HarshVegada <58640315+harshvegada@users.noreply.github.com>
2020-11-18 14:52:03 +0530

    Update HomePageTest.java

--- src/test/java/testScript/HomePageTest.java
+++ src/test/java/testScript/HomePageTest.java
@@ -7,60 +7,20 @@ import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
 
 import dataProvider.UserDataProvider;
-import io.qameta.allure.Description;
 
-@Listeners(base.TestNGListenersImpl.class)
+//@Listeners(base.TestNGListenersImpl.class)
 public class HomePageTest extends TestBase {
 
-	// @Description("Test Description: Submit Form username and password and email")
-	// @Test(dataProvider="dataSupplierMap")
-	// public void test1(String genderTitle, String firstName, String lastName,
-	// String emailAddress, String password,
-	// String dob, String userCity, String userAddress1, String zipCode, String
-	// userCountry, String homePhone,
-	// String mobilePhone) {
-	// System.out.println("Test Data");
-	// }
-
-	@Description("Test Description: Submit Form username and password and email")
 	@Test(dataProvider = "dataSupplierMap")
 	public void test1(Map<Object, Object> userDetails) {
-		commonFunction.signIn().createAccountForNewUser(userDetails.get("email").toString()).
-		createAccount(userDetails).
-		verifyMyAccountPageDisplay().
-		clickOnHomeButtonOnMyAccountPage().
-		gotoCommonFunction().
-		mouseHoverToWomenSectionAndClickOn(userDetails.get("productName").toString()).
-		displayTotalNumberResult().
-		choseFirstAvailableProduct().
-		enterQuantity("10").
-		clickOnAddToCartButton().
-		clickOnProceedToCheckout().
-		verifyTotalBillAmt().
-		verifyDeliveryAddress(userDetails).
-		clickOnProcessOnSummary().
-		clickOnProcessAddress().
-		agressToTermsAndConditions().
-		clickOnProcessOnCarrier().
-		payBillUsingCheck().
-		confirmOrder().
-		backToOrder();
-		
-		
-		
-		
-		
+		commonFunction.signIn().createAccountForNewUser(userDetails.get("email").toString()).createAccount(userDetails)
+				.verifyMyAccountPageDisplay().clickOnHomeButtonOnMyAccountPage().gotoCommonFunction()
+				.mouseHoverToWomenSectionAndClickOn(userDetails.get("productName").toString())
+				.displayTotalNumberResult().choseFirstAvailableProduct().enterQuantity("10").clickOnAddToCartButton()
+				.clickOnProceedToCheckout().verifyTotalBillAmt().verifyDeliveryAddress(userDetails)
+				.clickOnProcessOnSummary().clickOnProcessAddress().agressToTermsAndConditions()
+				.clickOnProcessOnCarrier().payBillUsingCheck().confirmOrder().backToOrder();
 	}
-
-	// @Description("Test Description: Submit Form username and password")
-	// @Test(description = "Submit Form username and password")
-	// public void test2() {
-	// }
-	//
-	// @Description("Test Description: Submit form with username and password exp")
-	// @Test(description = "Submit form with username and password exp")
-	// public void test3() {
-	// }
 
 	@DataProvider(name = "dataSupplierMap")
 	public Object[][] getTestData() {
