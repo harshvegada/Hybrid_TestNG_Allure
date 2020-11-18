b72ed47174ffa47e978953e9492a02692c81c40e
HarshVegada <58640315+harshvegada@users.noreply.github.com>
2020-11-18 14:53:05 +0530

    Update Constants.java

--- src/main/java/filePaths/Constants.java
+++ src/main/java/filePaths/Constants.java
@@ -19,19 +19,20 @@ public class Constants {
 	public static final String TEST_DATA = System.getProperty("user.dir") + File.separator + "src" + File.separator
 			+ "main" + File.separator + "resources" + File.separator + "testData" + File.separator + "userDetails.xlsx";
 
-	public static final String MYACCOUNT_ELEMENT = System.getProperty("user.dir") + File.separator + "src" + File.separator
-			+ "main" + File.separator + "resources" + File.separator + "locatorsPropertiesFile" + File.separator
-			+ "myAccount.properties";	
-	
-	public static final String PRODUCT_DETAILS_ELEMENT = System.getProperty("user.dir") + File.separator + "src" + File.separator
-			+ "main" + File.separator + "resources" + File.separator + "locatorsPropertiesFile" + File.separator
-			+ "productDetails.properties";	
-	
-	public static final String SHIPPING_AND_PAYMENT_ELEMENT = System.getProperty("user.dir") + File.separator + "src" + File.separator
-			+ "main" + File.separator + "resources" + File.separator + "locatorsPropertiesFile" + File.separator
-			+ "shippingAndPayment.properties";	
-	
-	
+	public static final String RANDOM_TEST_DATA = System.getProperty("user.dir") + File.separator + "random.xlsx";
+
+	public static final String MYACCOUNT_ELEMENT = System.getProperty("user.dir") + File.separator + "src"
+			+ File.separator + "main" + File.separator + "resources" + File.separator + "locatorsPropertiesFile"
+			+ File.separator + "myAccount.properties";
+
+	public static final String PRODUCT_DETAILS_ELEMENT = System.getProperty("user.dir") + File.separator + "src"
+			+ File.separator + "main" + File.separator + "resources" + File.separator + "locatorsPropertiesFile"
+			+ File.separator + "productDetails.properties";
+
+	public static final String SHIPPING_AND_PAYMENT_ELEMENT = System.getProperty("user.dir") + File.separator + "src"
+			+ File.separator + "main" + File.separator + "resources" + File.separator + "locatorsPropertiesFile"
+			+ File.separator + "shippingAndPayment.properties";
+
 	public static final String HOME_PAGE_TITLE = "My Store";
 	public static final String MY_ACCOUNT_TITLE = "MY ACCOUNT";
 
